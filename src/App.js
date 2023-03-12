77b2b2f27ee59205c3263d08cc60e49ca2aaf52b
Shivam Kumar <shivkumar386112@gmail.com>
2023-03-12 21:35:48 +0530

    removed tailwind and added react-router-dom react-player react-loader-spinner use-debounce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import {Footer} from './components/Footer';
+import {Navbar} from './components/Navbar';
+import {Routes} from './components/Routes';
+import {Link} from 'react-router-dom';
+import './index.css';
 const App = () => {
+
+    // const systemPrefTheme=window.matchMedia("(prefers-color-scheme: dark)")?'dark':'light';
+    // console.log("system theme is ",systemPrefTheme);
+
+    const [darkTheme,setDarkTheme]=useState(false);
+    
   return (
     <div>
-      <h1>Hello World</h1>
+        <div>
+            <Navbar/>
+            <Routes/>
+            <Footer/>
+        </div>
     </div>
   )
 }
 
-export default App
+export default App;
